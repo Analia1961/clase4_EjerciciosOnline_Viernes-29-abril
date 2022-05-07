1104055c10b03eb717d14f682e920c90e86f0796
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-05-07 05:02:23 -0300

    última corrección Ejercicio revisado para entregar al tutor Federico Arambillet

--- src/ejercicio1.ts
+++ src/ejercicio1.ts
@@ -1,20 +1,19 @@
 // EJERCICIO 1 - Mayor a 20 - Clase4 viernes 29 abril
 
-/* EJERCICIO EN LÍNEA
+/* EJERCICIO EN LÍNEA 
 
 rotulo.innerHTML = ”Escriba el número que desea verificar
- si es mayor o no a 20: “;*/
+ si es mayor o no a 20:"; */
 
 let dato = document.getElementById("dato");
 let btnEnviar = document.getElementById("enviar");
 
-btnEnviar.addEventListener("click" , () => {
-
+btnEnviar.addEventListener("click", () => {
   let datoExtraido: number = Number(dato.value);
 
   if (datoExtraido > 20) {
     console.log("El numero es mayor a 20");
   } else {
-    console.log("el numero es menor o igual que 20")
+    console.log("el numero es menor o igual que 20");
   }
-})
+});
